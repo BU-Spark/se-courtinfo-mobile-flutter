b86e71c0b7d4b5ed8bb747bd9546b849d7ff0c0d
ZixuanShu <60206922+ZixuanShu@users.noreply.github.com>
2021-10-26 21:54:08 -0400

    Navigation, validation done

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:scdao_mobile/constants/color_constants.dart';
@@ -8,131 +9,163 @@ class LoginScreen extends StatefulWidget {
 }
 
 class _LoginScreenState extends State<LoginScreen> {
+  final _text = TextEditingController();
+  final _emailtext = TextEditingController();
+  bool _validate = false;
+  bool _emailvalidate = false;
+  TextStyle defaultStyle = TextStyle(color: Colors.grey, fontSize: 20.0);
+  TextStyle linkStyle = TextStyle(color: Colors.yellow);
+  @override
+  void dispose() {
+    _emailtext.dispose();
+    _text.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       resizeToAvoidBottomInset: false,
       backgroundColor: Colors.white.withOpacity(0),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          SizedBox(height: 75,),
-          Image(
-            image: AssetImage("lib/assets/scdao-logo.png"),
-            height: 125.0,
-          ),
-          SizedBox(height: 45,),
-          Padding(
-            padding: const EdgeInsets.all(15.0),
-            child: TextField(
-              decoration: InputDecoration(
-                enabledBorder: UnderlineInputBorder(
-                  borderSide: BorderSide(color: Colors.white),
-                ),
-                focusedBorder: UnderlineInputBorder(
-                  borderSide: BorderSide(color: Colors.blueAccent),
-                ),
-                errorBorder: UnderlineInputBorder(
-                  borderSide: BorderSide(color: Colors.redAccent),
-                ),
-                focusedErrorBorder: UnderlineInputBorder(
-                  borderSide: BorderSide(color: Colors.redAccent),
-                ),
-                disabledBorder: UnderlineInputBorder(
-                  borderSide: BorderSide(color: Colors.grey),
-                ),
-                hintStyle: TextStyle(color: Colors.white),
-                hintText: "Email",
-                prefixIcon: Icon(
-                  FontAwesomeIcons.mailBulk,
-                  color: Colors.white,
+      body: FilteredBackgroundImage(
+        colorFilterColor: Color(0x441F2C5C),
+        image: AssetImage('lib/assets/login_background.png'),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            SizedBox(
+              height: 75,
+            ),
+            Image(
+              image: AssetImage("lib/assets/scdao-logo.png"),
+              height: 125.0,
+            ),
+            SizedBox(
+              height: 45,
+            ),
+            Padding(
+              padding: const EdgeInsets.all(15.0),
+              child: TextField(
+                style: TextStyle(color: Colors.white),
+                controller: _text,
+                decoration: InputDecoration(
+                  errorText: _validate ? 'Username Can\'t Be Empty' : null,
+                  enabledBorder: UnderlineInputBorder(
+                    borderSide: BorderSide(color: Colors.white),
+                  ),
+                  focusedBorder: UnderlineInputBorder(
+                    borderSide: BorderSide(color: Colors.blueAccent),
+                  ),
+                  errorBorder: UnderlineInputBorder(
+                    borderSide: BorderSide(color: Colors.redAccent),
+                  ),
+                  focusedErrorBorder: UnderlineInputBorder(
+                    borderSide: BorderSide(color: Colors.redAccent),
+                  ),
+                  disabledBorder: UnderlineInputBorder(
+                    borderSide: BorderSide(color: Colors.grey),
+                  ),
+                  hintStyle: TextStyle(color: Colors.grey),
+                  hintText: "Username",
+                  prefixIcon: Icon(
+                    FontAwesomeIcons.mailBulk,
+                    color: Colors.white,
+                  ),
                 ),
               ),
             ),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(15.0),
-            child: TextField(
-              obscureText: true,
-              decoration: InputDecoration(
-                enabledBorder: UnderlineInputBorder(
-                  borderSide: BorderSide(color: Colors.white),
-                ),
-                focusedBorder: UnderlineInputBorder(
-                  borderSide: BorderSide(color: Colors.blueAccent),
-                ),
-                errorBorder: UnderlineInputBorder(
-                  borderSide: BorderSide(color: Colors.redAccent),
-                ),
-                focusedErrorBorder: UnderlineInputBorder(
-                  borderSide: BorderSide(color: Colors.redAccent),
-                ),
-                disabledBorder: UnderlineInputBorder(
-                  borderSide: BorderSide(color: Colors.grey),
-                ),
-                hintStyle: TextStyle(color: Colors.white),
-                hintText: "Password",
-                prefixIcon: Icon(FontAwesomeIcons.userLock, color: Colors.white,),
-              ),
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(15.0),
-            child: TextField(
-              obscureText: true,
-              decoration: InputDecoration(
-                enabledBorder: UnderlineInputBorder(
-                  borderSide: BorderSide(color: Colors.white),
-                ),
-                focusedBorder: UnderlineInputBorder(
-                  borderSide: BorderSide(color: Colors.blueAccent),
-                ),
-                errorBorder: UnderlineInputBorder(
-                  borderSide: BorderSide(color: Colors.redAccent),
-                ),
-                focusedErrorBorder: UnderlineInputBorder(
-                  borderSide: BorderSide(color: Colors.redAccent),
-                ),
-                disabledBorder: UnderlineInputBorder(
-                  borderSide: BorderSide(color: Colors.grey),
-                ),
-                hintStyle: TextStyle(color: Colors.white),
-                hintText: "Confirm Password",
-                prefixIcon: Icon(FontAwesomeIcons.userLock, color: Colors.white,),
-              ),
-            ),
-          ),
-          SizedBox(height: 100),
-          Text(
-            "Already have an account? Login",
-            style: TextStyle(color: Colors.white),
-          ),
-          SizedBox(
-            height: 25.0,
-          ),
-          TextButton(
-            onPressed: () {},
-            child: Text(
-              "Sign Up",
-              style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 35.0,
-                  fontWeight: FontWeight.w600),
-            ),
-            style: ButtonStyle(
-              fixedSize: MaterialStateProperty.all(Size.fromWidth(320)),
-              backgroundColor: MaterialStateProperty.all(kscdaoYellow),
-              shape: MaterialStateProperty.all(
-                RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(20),
+            Padding(
+              padding: const EdgeInsets.all(15.0),
+              child: TextField(
+                obscureText: true,
+                controller: _emailtext,
+                style: TextStyle(color: Colors.white),
+                decoration: InputDecoration(
+                  errorText: _emailvalidate ? 'Password Can\'t Be Empty' : null,
+                  enabledBorder: UnderlineInputBorder(
+                    borderSide: BorderSide(color: Colors.white),
+                  ),
+                  focusedBorder: UnderlineInputBorder(
+                    borderSide: BorderSide(color: Colors.blueAccent),
+                  ),
+                  errorBorder: UnderlineInputBorder(
+                    borderSide: BorderSide(color: Colors.redAccent),
+                  ),
+                  focusedErrorBorder: UnderlineInputBorder(
+                    borderSide: BorderSide(color: Colors.redAccent),
+                  ),
+                  disabledBorder: UnderlineInputBorder(
+                    borderSide: BorderSide(color: Colors.grey),
+                  ),
+                  hintStyle: TextStyle(color: Colors.grey),
+                  hintText: "Password",
+                  prefixIcon: Icon(
+                    FontAwesomeIcons.userLock,
+                    color: Colors.white,
+                  ),
                 ),
               ),
             ),
-          ),
-          SizedBox(
-            height: 100.0,
-          )
-        ],
+            Column(
+              children: <Widget>[
+                Align(
+                  alignment: Alignment.centerRight,
+                  child: Container(
+                    child: Text(
+                      "Forgot your password?",
+                      style: TextStyle(color: Colors.grey),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(height: 100),
+            RichText(
+              text: TextSpan(
+                style: defaultStyle,
+                children: <TextSpan>[
+                  TextSpan(text: 'Forgot your password? '),
+                  TextSpan(
+                      text: 'Sign up',
+                      style: linkStyle,
+                      recognizer: TapGestureRecognizer()
+                        ..onTap = () =>
+                            {Navigator.of(context).pushNamed('SignupPage')}),
+                ],
+              ),
+            ),
+            SizedBox(
+              height: 25.0,
+            ),
+            TextButton(
+              onPressed: () {
+                setState(() {
+                  _validate = _text.text.isEmpty;
+                  _emailvalidate = _emailtext.text.isEmpty;
+                });
+              },
+              child: Text(
+                "Log In",
+                style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 35.0,
+                    fontWeight: FontWeight.w600),
+              ),
+              style: ButtonStyle(
+                fixedSize: MaterialStateProperty.all(Size.fromWidth(320)),
+                backgroundColor: MaterialStateProperty.all(kscdaoYellow),
+                shape: MaterialStateProperty.all(
+                  RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(20),
+                  ),
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 100.0,
+            )
+          ],
+        ),
       ),
     );
   }
--- lib/screens/signup_screen.dart
+++ lib/screens/signup_screen.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:scdao_mobile/constants/color_constants.dart';
@@ -8,15 +9,23 @@ class SignupScreen extends StatefulWidget {
 }
 
 class _SignupScreenState extends State<SignupScreen> {
-  final _text = TextEditingController();
+  final _userNametext = TextEditingController();
+  final _passwordtext = TextEditingController();
   final _emailtext = TextEditingController();
-  bool _validate = false;
-  bool _emailvalidate = false;
+  final _confirmPtext = TextEditingController();
+  bool _email = false;
+  bool _confirmP = false;
+  bool _userName = false;
+  bool _passwordvalidate = false;
+  TextStyle defaultStyle = TextStyle(color: Colors.grey, fontSize: 20.0);
+  TextStyle linkStyle = TextStyle(color: Colors.yellow);
 
   @override
   void dispose() {
     _emailtext.dispose();
-    _text.dispose();
+    _confirmPtext.dispose();
+    _passwordtext.dispose();
+    _userNametext.dispose();
     super.dispose();
   }
 
@@ -25,131 +34,199 @@ class _SignupScreenState extends State<SignupScreen> {
     return Scaffold(
       resizeToAvoidBottomInset: false,
       backgroundColor: Colors.white.withOpacity(0),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          SizedBox(
-            height: 75,
-          ),
-          Image(
-            image: AssetImage("lib/assets/scdao-logo.png"),
-            height: 125.0,
-          ),
-          SizedBox(
-            height: 45,
-          ),
-          Padding(
-            padding: const EdgeInsets.all(15.0),
-            child: TextField(
-              style: TextStyle(color: Colors.white),
-              controller: _text,
-              decoration: InputDecoration(
-                errorText: _validate ? 'Username Can\'t Be Empty' : null,
-                enabledBorder: UnderlineInputBorder(
-                  borderSide: BorderSide(color: Colors.white),
-                ),
-                focusedBorder: UnderlineInputBorder(
-                  borderSide: BorderSide(color: Colors.blueAccent),
-                ),
-                errorBorder: UnderlineInputBorder(
-                  borderSide: BorderSide(color: Colors.redAccent),
-                ),
-                focusedErrorBorder: UnderlineInputBorder(
-                  borderSide: BorderSide(color: Colors.redAccent),
-                ),
-                disabledBorder: UnderlineInputBorder(
-                  borderSide: BorderSide(color: Colors.grey),
-                ),
-                hintStyle: TextStyle(color: Colors.grey),
-                hintText: "Username",
-                prefixIcon: Icon(
-                  FontAwesomeIcons.mailBulk,
-                  color: Colors.white,
-                ),
-              ),
+      body: FilteredBackgroundImage(
+        colorFilterColor: Color(0x441F2C5C),
+        image: AssetImage('lib/assets/login_background.png'),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            SizedBox(
+              height: 50,
             ),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(15.0),
-            child: TextField(
-              obscureText: true,
-              controller: _emailtext,
-              style: TextStyle(color: Colors.white),
-              decoration: InputDecoration(
-                errorText: _emailvalidate ? 'Password Can\'t Be Empty' : null,
-                enabledBorder: UnderlineInputBorder(
-                  borderSide: BorderSide(color: Colors.white),
-                ),
-                focusedBorder: UnderlineInputBorder(
-                  borderSide: BorderSide(color: Colors.blueAccent),
-                ),
-                errorBorder: UnderlineInputBorder(
-                  borderSide: BorderSide(color: Colors.redAccent),
-                ),
-                focusedErrorBorder: UnderlineInputBorder(
-                  borderSide: BorderSide(color: Colors.redAccent),
-                ),
-                disabledBorder: UnderlineInputBorder(
-                  borderSide: BorderSide(color: Colors.grey),
-                ),
-                hintStyle: TextStyle(color: Colors.grey),
-                hintText: "Password",
-                prefixIcon: Icon(
-                  FontAwesomeIcons.userLock,
-                  color: Colors.white,
-                ),
-              ),
+            Image(
+              image: AssetImage("lib/assets/scdao-logo.png"),
+              height: 125.0,
             ),
-          ),
-          Column(
-            children: <Widget>[
-              Align(
-                alignment: Alignment.centerRight,
-                child: Container(
-                  child: Text(
-                    "Forgot your password?",
-                    style: TextStyle(color: Colors.grey),
+            SizedBox(
+              height: 15,
+            ),
+            Padding(
+              padding: const EdgeInsets.all(15.0),
+              child: TextField(
+                style: TextStyle(color: Colors.white),
+                controller: _emailtext,
+                decoration: InputDecoration(
+                  errorText: _email ? 'Email Can\'t Be Empty' : null,
+                  enabledBorder: UnderlineInputBorder(
+                    borderSide: BorderSide(color: Colors.white),
+                  ),
+                  focusedBorder: UnderlineInputBorder(
+                    borderSide: BorderSide(color: Colors.blueAccent),
+                  ),
+                  errorBorder: UnderlineInputBorder(
+                    borderSide: BorderSide(color: Colors.redAccent),
+                  ),
+                  focusedErrorBorder: UnderlineInputBorder(
+                    borderSide: BorderSide(color: Colors.redAccent),
+                  ),
+                  disabledBorder: UnderlineInputBorder(
+                    borderSide: BorderSide(color: Colors.grey),
+                  ),
+                  hintStyle: TextStyle(color: Colors.grey),
+                  hintText: "Email",
+                  prefixIcon: Icon(
+                    FontAwesomeIcons.mailBulk,
+                    color: Colors.white,
                   ),
                 ),
               ),
-            ],
-          ),
-          SizedBox(height: 100),
-          Text(
-            "Don't have an account? Sign up",
-            style: TextStyle(color: Colors.white),
-          ),
-          SizedBox(
-            height: 25.0,
-          ),
-          TextButton(
-            onPressed: () {
-              setState(() {
-                _validate = _text.text.isEmpty;
-                _emailvalidate = _emailtext.text.isEmpty;
-              });
-            },
-            child: Text(
-              "Log In",
-              style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 35.0,
-                  fontWeight: FontWeight.w600),
             ),
-            style: ButtonStyle(
-              fixedSize: MaterialStateProperty.all(Size.fromWidth(320)),
-              backgroundColor: MaterialStateProperty.all(kscdaoYellow),
-              shape: MaterialStateProperty.all(
-                RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(20),
+            Padding(
+              padding: const EdgeInsets.all(15.0),
+              child: TextField(
+                style: TextStyle(color: Colors.white),
+                controller: _userNametext,
+                decoration: InputDecoration(
+                  errorText: _userName ? 'Username Can\'t Be Empty' : null,
+                  enabledBorder: UnderlineInputBorder(
+                    borderSide: BorderSide(color: Colors.white),
+                  ),
+                  focusedBorder: UnderlineInputBorder(
+                    borderSide: BorderSide(color: Colors.blueAccent),
+                  ),
+                  errorBorder: UnderlineInputBorder(
+                    borderSide: BorderSide(color: Colors.redAccent),
+                  ),
+                  focusedErrorBorder: UnderlineInputBorder(
+                    borderSide: BorderSide(color: Colors.redAccent),
+                  ),
+                  disabledBorder: UnderlineInputBorder(
+                    borderSide: BorderSide(color: Colors.grey),
+                  ),
+                  hintStyle: TextStyle(color: Colors.grey),
+                  hintText: "Username",
+                  prefixIcon: Icon(
+                    FontAwesomeIcons.mailBulk,
+                    color: Colors.white,
+                  ),
                 ),
               ),
             ),
-          ),
-          SizedBox(
-            height: 100.0,
-          )
-        ],
+            Padding(
+              padding: const EdgeInsets.all(15.0),
+              child: TextField(
+                obscureText: true,
+                controller: _passwordtext,
+                style: TextStyle(color: Colors.white),
+                decoration: InputDecoration(
+                  errorText:
+                      _passwordvalidate ? 'Password Can\'t Be Empty' : null,
+                  enabledBorder: UnderlineInputBorder(
+                    borderSide: BorderSide(color: Colors.white),
+                  ),
+                  focusedBorder: UnderlineInputBorder(
+                    borderSide: BorderSide(color: Colors.blueAccent),
+                  ),
+                  errorBorder: UnderlineInputBorder(
+                    borderSide: BorderSide(color: Colors.redAccent),
+                  ),
+                  focusedErrorBorder: UnderlineInputBorder(
+                    borderSide: BorderSide(color: Colors.redAccent),
+                  ),
+                  disabledBorder: UnderlineInputBorder(
+                    borderSide: BorderSide(color: Colors.grey),
+                  ),
+                  hintStyle: TextStyle(color: Colors.grey),
+                  hintText: "Password",
+                  prefixIcon: Icon(
+                    FontAwesomeIcons.userLock,
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(15.0),
+              child: TextField(
+                obscureText: true,
+                controller: _confirmPtext,
+                style: TextStyle(color: Colors.white),
+                decoration: InputDecoration(
+                  errorText: _confirmP ? 'Different input Passwords' : null,
+                  enabledBorder: UnderlineInputBorder(
+                    borderSide: BorderSide(color: Colors.white),
+                  ),
+                  focusedBorder: UnderlineInputBorder(
+                    borderSide: BorderSide(color: Colors.blueAccent),
+                  ),
+                  errorBorder: UnderlineInputBorder(
+                    borderSide: BorderSide(color: Colors.redAccent),
+                  ),
+                  focusedErrorBorder: UnderlineInputBorder(
+                    borderSide: BorderSide(color: Colors.redAccent),
+                  ),
+                  disabledBorder: UnderlineInputBorder(
+                    borderSide: BorderSide(color: Colors.grey),
+                  ),
+                  hintStyle: TextStyle(color: Colors.grey),
+                  hintText: "Confirm Password",
+                  prefixIcon: Icon(
+                    FontAwesomeIcons.userLock,
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+            ),
+            SizedBox(height: 25),
+            RichText(
+              text: TextSpan(
+                style: defaultStyle,
+                children: <TextSpan>[
+                  TextSpan(text: 'Already have an account? '),
+                  TextSpan(
+                      text: 'Login',
+                      style: linkStyle,
+                      recognizer: TapGestureRecognizer()
+                        ..onTap = () =>
+                            {Navigator.of(context).pushNamed('loginPage')}),
+                ],
+              ),
+            ),
+            SizedBox(
+              height: 25.0,
+            ),
+            TextButton(
+              onPressed: () {
+                setState(() {
+                  _userName = _userNametext.text.isEmpty;
+                  _passwordvalidate = _passwordtext.text.isEmpty;
+                  _email = _emailtext.text.isEmpty;
+                  if (_passwordtext.text != _confirmPtext.text)
+                    _confirmP = true;
+                });
+              },
+              child: Text(
+                "Sign Up",
+                style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 35.0,
+                    fontWeight: FontWeight.w600),
+              ),
+              style: ButtonStyle(
+                fixedSize: MaterialStateProperty.all(Size.fromWidth(320)),
+                backgroundColor: MaterialStateProperty.all(kscdaoYellow),
+                shape: MaterialStateProperty.all(
+                  RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(20),
+                  ),
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 100.0,
+            )
+          ],
+        ),
       ),
     );
   }
