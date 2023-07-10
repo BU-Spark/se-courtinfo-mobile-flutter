6fcbc688dfe15736d57760ff91a26a988fa6de9a
MollyZhou <mollyzhou001@gmail.com>
2023-07-10 14:46:09 -0400

    getUser & removeUser deleted

--- lib/utility/shared_pref.dart
+++ lib/utility/shared_pref.dart
@@ -11,23 +11,6 @@ class UserPreferences {
     return prefs.commit();
   }
 
-  Future<Token> getUser() async {
-    final SharedPreferences prefs = await SharedPreferences.getInstance();
-
-    String type = prefs.getString("type") ?? "";
-    String token = prefs.getString("token") ?? "";
-
-    return Token(
-        tokenType: type, 
-        accessToken: token);
-  }
-
-  void removeUser() async {
-    final SharedPreferences prefs = await SharedPreferences.getInstance();
-
-    prefs.remove('type');
-    prefs.remove('token');
-  }
 
   Future<String> getToken() async {
     final SharedPreferences prefs = await SharedPreferences.getInstance();
